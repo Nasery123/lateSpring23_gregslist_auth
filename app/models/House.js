723b3be1a89bb598f679e0be976c2423ed86b49f
Abdul Rauof Nasery <rauf.nasery143@gmail.com>
2023-05-28 15:10:37 -0600

    almost finished

--- app/models/House.js
+++ app/models/House.js
@@ -54,7 +54,7 @@ export class House {
 
   static HouseForm() {
     return /*html*/`
-      < div class=" modal-header flex-end" >
+
         <h1 class="modal-title fs-5" id="exampleModalLabel">LIST NEW HOUSE </h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </ >
